ff94b6fe571d0392ee2328a1c48674ad55ab224b
AryoDjat1 <aryodjati5@gmail.com>
2023-06-24 20:30:44 +0700

    add API Client

--- app/src/main/java/com/djatscode/suggestioncourse/network/APIClient.kt
+++ app/src/main/java/com/djatscode/suggestioncourse/network/APIClient.kt
@@ -4,8 +4,15 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class APIClient {
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl("https://api.openai.com/v1/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+    companion object{
+        private const val BASE_URL = "https://api.openai.com/v1/"
+
+        fun getClient(): Retrofit {
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
+
 }

69506f9c6764ba1b6698a09a7f2ddb26da132370
AryoDjat1 <aryodjati5@gmail.com>
2023-06-24 19:51:53 +0700

    setup API Service Interface

---
+++ app/src/main/java/com/djatscode/suggestioncourse/network/APIService.kt
@@ -0,0 +1,15 @@
+package com.djatscode.suggestioncourse.network
+
+import com.djatscode.suggestioncourse.model.CompletionResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface APIService {
+    @Headers("Content-Type: application/json")
+    @POST("openai/completions")
+    fun complete(
+        @Body request:
+    ) : Call<CompletionResponse>
+}
